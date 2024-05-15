3b1227de8abd5e5ba7727998122f3ea8f3a6fba1
TakahiroYamamoto90 <76931295+TakahiroYamamoto90@users.noreply.github.com>
2024-05-15 04:29:56 +0000

    Use SSL and the Puma web server in production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-#bundle exec rails db:migrate
-bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rake db:schema:load
+bundle exec rails db:migrate
+#bundle exec rails db:migrate RAILS_ENV=production
