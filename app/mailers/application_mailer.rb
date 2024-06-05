aab5b3b79f7dd8aaa0c0eae61a8a914a37797498
TakahiroYamamoto90 <76931295+TakahiroYamamoto90@users.noreply.github.com>
2024-06-05 14:21:16 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "postmaster@sandboxffe2db8b555f4ccb8bd5d9b69d91962c.mailgun.org"
   layout "mailer"
 end
